2cedb57bd8c3ca168757a70a37a9c6114155e4fc
Damon R <dmarcr1997@gmail.com>
2020-01-29 22:23:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     new_string = []
-    new_string = new_string.split(/[.?! ]/) 
+    new_string = self.split(/[.?!] /) 
     binding.pry
     new_string.length
   end
